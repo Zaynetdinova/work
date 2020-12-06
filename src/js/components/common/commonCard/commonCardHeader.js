eb6283da9a4cd347df1ce30bc1b5e603e5bdcbab
ilmirazaynetdinova <ilmirazainetdinova686@gmail.com>
2020-12-06 19:35:03 +0300

    Dots and anather changes

--- src/js/components/common/commonCard/commonCardHeader.js
+++ src/js/components/common/commonCard/commonCardHeader.js
@@ -13,8 +13,22 @@ export function commonCardHeader(text) {
         </div>
       </section>
       <section class="slider-pointer">
-        ${sliderPointerTemplate('slider-pointer-bestseller')}
+         ${sliderPointerTemplate('slider-pointer-bestseller')}
+         ${r()}
       </section>
     </div>
   `
 }
+
+function r () {
+  return `
+    <div class="dots">
+        <span class="dots-bestseller active"></span>
+        <span class="dots-bestseller"></span>
+        <span class="dots-bestseller"></span>
+        <span class="dots-bestseller"></span>
+    </div>
+  `
+}
+
+
