3cadbedfbbd4a13831152eda91209377336f0960
designer <moyo.group@iMac-designer.local>
2020-12-07 18:57:49 +0300

    new -page

--- src/js/pages/ViewCategoriesPage.js
+++ src/js/pages/ViewCategoriesPage.js
@@ -1,16 +1,11 @@
 import {Page} from '../core/Page'
 import {OptMoyo} from '../components/OptMoyo/OptMoyo'
 import {Header} from '../components/header/Header'
-import {Main} from '../components/main/Main'
-import {PopularBrand} from '../components/popularBrand/PopularBrand'
-import {SharesBlock} from '../components/sharesBlock/sharesBlock'
-import {MoyomodaBlock} from '../components/moyomodaBlock/MoyomodaBlock'
-import {Bestsellers} from '../components/bestsellers/Bestsellers'
 import {Footer} from '../components/footer/Footer'
 import {eventGlobal} from '../core/eventGlobal'
 import {CategoriesVideo} from '../components/categoriesVideo/categoriesVideo'
-import {TransitionTitle} from "../components/categoriesTransitionTitle/categoriesTitle";
-import {categoriesMenuSidebar} from "../components/categoriesMenuSidebar/categoriesMenuSidebar";
+import {TransitionTitle} from "../components/categoriesTransitionTitle/transitionTitle";
+import {PartnerOffers} from "../components/partnerOffers/PartnerOffers";
 
 export class ViewCategoriesPage extends Page{
 	getRoot() {
