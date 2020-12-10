56a93ec1e35d4760f1698e0370a1d501b803fae9
ilmirazaynetdinova <ilmirazainetdinova686@gmail.com>
2020-12-10 18:15:44 +0300

    mobile-menu-bug-fix

--- src/js/core/utils/openSidebar.js
+++ src/js/core/utils/openSidebar.js
@@ -1,38 +1,32 @@
 export function openSidebar() {
-  const test = document.querySelectorAll('.point-js')
+  // устанавливаем прослушку событий
+  const point = document.querySelectorAll('.point-js')
+  const cancel = document.querySelector('#sub-categories-menu-close')
 
-  test.forEach((item) => {
+  point.forEach((item) => {
     item.addEventListener('click', (event) =>  openCategories(event, item.dataset.type))
   })
 
-  const cancel = document.querySelector('#sub-categories-menu-close')
+  // закрывает подкатегорию
   cancel.addEventListener('click', closeSubCategories)
-}
-
-function closeSubCategories() {
-  const extra = document.querySelector('#open-mobile-extra-js')
-  extra.classList.remove('show-extra-mobile')
-  extra.classList.add('show-extra-cancel')
 }
 
 function openCategories(e, category) {
   const subCategories = e.target.closest('figure')
+  const mainCategory = document.querySelector(`[data-type=${category}]`)
 
-  if(document.documentElement.clientWidth <= 768 && subCategories && subCategories.dataset.subtest == undefined) {
-    const extra = document.querySelector('#open-mobile-extra-js')
-    extra.classList.remove('show-extra-cancel')
-    extra.classList.add('show-extra-mobile')
+  // открываем подкатегорию
+  if(document.documentElement.clientWidth <= 768 && subCategories && subCategories.dataset.subcategory == undefined) {
+    closeSubCategories('open')
+    return
   }
 
-  if (subCategories !== null && subCategories.dataset.test != undefined) {
+  // ????
+  if (subCategories !== null && subCategories.dataset.category != undefined) {
     subCategories.classList.toggle('show')
     return
-  } else if(subCategories !== null && subCategories.dataset.test2 != undefined) {
-    return
   }
 
-
-  const mainCategory = document.querySelector(`[data-type=${category}]`)
 
   if (mainCategory.classList.contains('open-category-js')) {
     close()
@@ -42,7 +36,27 @@ function openCategories(e, category) {
   }
 }
 
+export function closeSidebarCloseMenu() {
+  close()
+  closeSubCategories('close')
+}
 
+
+function closeSubCategories(type) {
+  const extra = document.querySelector('#open-mobile-extra-js')
+  if(type === 'open') {
+    extra.classList.remove( 'show-extra-cancel')
+    extra.classList.add('show-extra-mobile')
+  } else if(type === 'close') {
+    extra.classList.remove('show-extra-mobile')
+    extra.classList.remove('show-extra-cancel')
+  } else {
+    extra.classList.remove('show-extra-mobile')
+    extra.classList.add('show-extra-cancel')
+  }
+}
+
+// закрывает все открытые категории
 function close() {
   const elements = document.querySelectorAll('#point-js')
   elements.forEach((item) => {
