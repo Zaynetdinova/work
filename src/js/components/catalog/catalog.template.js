a55e5c90b90188b91743f9e86a99d19076a80c1e
designer <moyo.group@iMac-designer.local>
2020-12-11 18:31:54 +0300

    PageNewCatalog

--- src/js/components/catalog/catalog.template.js
+++ src/js/components/catalog/catalog.template.js
@@ -1,6 +1,8 @@
 import {titleCatalog, transitionTitleCatalog} from "./js/transitionTitleCatalog";
 import {number, pagination} from "./js/pagination";
-import {categoriesMenuSidebar} from "../common/categoriesMenuSidebar";
+import {sidebarPoint} from "../common/sidebarPoint";
+import {navCatalog} from "./js/navCatalog";
+import {filter} from "./js/filter";
 
 
 export function catalog()  {
@@ -13,10 +15,14 @@ export function catalog()  {
                 ${number()}
             </div>
             <div class="flex-wrap">	
-				<div class="sidebar-page-categories">				
-					
+				<div class="sidebar-page-categories">
+                    <div class="points">				
+                        ${sidebarPoint(point,'notArrow')}
+                    </div>
+                    <div class="wrapper-filter">${filter()}</div>                    
 				</div>
-				<div class="content-page-categories">	
+				<div class="content-page-categories">
+				    ${navCatalog(item)}
     			</div>
   			</div>                  
         </div>
@@ -33,3 +39,72 @@ let catalogCategory = [
         linkCategory: '/#categories-view'
     }
 ]
+
+let point =
+    [
+        {
+            title: 'Платья',
+            id: 'new1',
+            img: false,
+            category: [
+                {
+                    category: 'Повседневные'
+                },
+                {
+                    category: 'Офисные'
+                },
+                {
+                    category: 'Трикотажные'
+                },
+                {
+                    category: 'Коктейльные'
+                },
+                {
+                    category: 'Вечерние'
+                },
+                {
+                    category: 'Длинные'
+                },
+                {
+                    category: 'Костюмы'
+                },
+                {
+                    category: 'Сарафаны'
+                },
+                {
+                    category: 'Летние'
+                },
+                {
+                    category: 'Короткие'
+                },
+            ],
+        },
+]
+
+let item = [
+    {
+        title: 'Наш выбор'
+    },
+    {
+        title: 'Дешевые'
+    },
+    {
+        title: 'Дорогие'
+    },
+    {
+        title: 'Новые'
+    },
+    {
+        title: 'Старые'
+    },
+    {
+        title: 'Скидки'
+    },
+    {
+        title: 'Акции'
+    },
+    {
+        title: 'Популярные'
+    },
+]
+
