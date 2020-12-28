286441bb5b432d2cf0aab7904ea58c8b9e12c0cf
designer <moyo.group@iMac-designer.local>
2020-12-28 18:00:10 +0300

    card-product-page-3

--- src/js/components/cardProductPage/view/form.js
+++ src/js/components/cardProductPage/view/form.js
@@ -21,7 +21,7 @@ export function form() {
                         <div class="size"><input id="3" class="input" type="radio" name="topic1"><label for="3">Большемерит</label></div>
                     </div>
                 </div>            
-                <input class="button-question" type="text" placeholder="Ваш вопрос или отзыв (не обязательно)">
+                <textarea class="button-question" placeholder="Ваш вопрос или отзыв (не обязательно)"></textarea>
             
                 <div id="drop-area-js" class="drop-area">
                   <form>
