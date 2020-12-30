558b6ca4144f1bb1d5442a296179c500e843363f
designer <moyo.group@iMac-designer.local>
2020-12-30 15:25:04 +0300

    finish-style

--- src/js/components/cardProductPage/view/favorites.js
+++ src/js/components/cardProductPage/view/favorites.js
@@ -1,6 +1,7 @@
 export function favorites() {
     return `
-        <article id="favorites-product-js" class="favoritesProduct">           
+        <article id="favorites-product-js" class="favoritesProduct">
+            <div id="elem" class="text">Добавить в избранное</div>           
             <div class="img-like"></div>
         </article>            
     `
