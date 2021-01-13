02bd4185d30a6abbda442cb29eaaa5bdd24b90a0
designer <moyo.group@iMac-designer.local>
2021-01-13 14:33:00 +0300

    otziv

--- src/js/components/cardProductPage/js/showForm.js
+++ src/js/components/cardProductPage/js/showForm.js
@@ -2,8 +2,13 @@ export function showForm(type = '') {
 	const $el = document.querySelector('#regulate-form-and-statistics-js')
 
 	$el.classList.add('show-form')
+	$el.classList.remove('gratitude-form')
 
 	if(type === 'cancel') {
+		$el.classList.remove('show-form')
+	}
+	if (type === 'send') {
+		$el.classList.add('gratitude-form')
 		$el.classList.remove('show-form')
 	}
 }
