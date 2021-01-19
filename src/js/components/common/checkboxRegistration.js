328880fc36659df5361dfb1485f97aaa3e498889
designer <moyo.group@iMac-designer.local>
2021-01-19 18:07:28 +0300

    registration

---
+++ src/js/components/common/checkboxRegistration.js
@@ -0,0 +1,12 @@
+export function checkboxRegistration() {
+    return `
+        <div class="wrap-checkbox">
+            <input class="checkbox checkbox-filter-js" type="checkbox" id="forMe" data-text="Даю согласие на обработку персональных данных"> 
+            <label for="forMe">
+                <div class="text-checkbox">
+                Даю согласие на обработку персональных данных
+                </div>
+            </label>
+        </div> 
+    `
+}
