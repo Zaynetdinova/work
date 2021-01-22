60101321f12137a25f3305cea305cad7ca214f1d
designer <moyo.group@iMac-designer.local>
2021-01-22 18:37:41 +0300

    personal-basket

--- src/js/components/personalArea/view/basket.template.js
+++ src/js/components/personalArea/view/basket.template.js
@@ -1,14 +1,22 @@
+import women from '../../../../images/bestsellers/1/img1.png'
+
 export function basketTemplate() {
 	return `
 		<div class="Basket-table">
 			<table>
 				<tr>
-					<th class="first-string" style="width: 394px">Наименование</th>
+					<th class="first-string" style="width: 393px">Наименование</th>
 					<th class="first-string" style="width: 167px">Размер</th>
 					<th class="first-string" style="width: 186px">Количество</th>
 					<th class="first-string" style="width: 184px">Цена</th>
 					<th class="first-string" style="width: 179px">Скидка</th>
-					<th class="first-string" style="width: 330px">Сумма</th>
+					<th class="first-string" style="width: 331px">Сумма</th>
+				</tr>
+				<tr>
+					<td>
+						<div><img src="${women}" alt="" style="width: 128px;height: 193.03px;"></div>
+					</td>
+					<td></td>
 				</tr>
 			</table>
 		</div>
