b2d3500da7bdc60503fb3c395efed4e68be2abe6
ilmirazaynetdinova <ilmirazainetdinova686@gmail.com>
2021-01-24 21:53:13 +0300

    feature-form-phone-number

--- src/js/components/common/checkboxRegistration.js
+++ src/js/components/common/checkboxRegistration.js
@@ -1,12 +1,14 @@
 export function checkboxRegistration(text) {
     return `
-        <div class="wrap-checkbox">
-            <input required class="checkbox checkbox-filter-js" type="checkbox" id="forMe"> 
-            <label for="forMe">
-                <div class="text-checkbox">
-                    ${text}
-                </div>
-            </label>
-        </div> 
+        <form class="form-js">
+            <div class="wrap-checkbox">
+                <input required checked class="checkbox checkbox-filter-js" type="checkbox" id="forMe"> 
+                <label for="forMe">
+                    <div class="text-checkbox">
+                        ${text}
+                    </div>
+                </label>
+            </div> 
+        </form>
     `
 }
--- src/js/core/utils/inputMaskPhone.js
+++ src/js/core/utils/inputMaskPhone.js
@@ -1,7 +1,29 @@
 import Inputmask from 'inputmask'
 
-export function inputMaskPhone(selectorName) {
+export function inputMaskPhone(selectorName, country = '') {
+	let number
+
+	switch (country) {
+		case 'Russia':
+			number = '+7'
+			break
+		case 'Armenia':
+			number = '+374'
+			break
+		case 'Georgia':
+			number = '+\\9\\95'
+			break
+		case 'Kazakhstan':
+			number = '+7'
+			break
+		case 'Belarus':
+			number = '+375'
+			break
+		default:
+			number = '+7'
+	}
+
 	const selector = document.querySelector(selectorName);
-	const im = new Inputmask("+7 (999) -999-99-99");
+	const im = new Inputmask(`${number} (999) -999-99-99`)
 	return im.mask(selector);
 }
