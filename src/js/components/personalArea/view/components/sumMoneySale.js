b2a664cc77398bcebcdaf8a8d499c0e22cc013ff
designer <moyo.group@iMac-designer.local>
2021-01-26 18:59:46 +0300

    personal-area-delivery

--- src/js/components/personalArea/view/components/sumMoneySale.js
+++ src/js/components/personalArea/view/components/sumMoneySale.js
@@ -2,7 +2,8 @@ export function SumMoneySale(item) {
     const items = item.map((item) => {
     return `
         <div class="Sum-money-sale">
-            <span class="title-sum"><b>${item.title}</b></span>
+            <span class="title-sum">${item.title}</span>
+            <span class="line"></span>
             <span class="money-sum">${item.sum}</span>
         </div>
     `
