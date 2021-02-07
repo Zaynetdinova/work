abbeb8fc275a869a6c6f3f9a1b3bb2d91ea92ef4
ilmirazaynetdinova <ilmirazainetdinova686@gmail.com>
2021-02-07 20:19:22 +0300

    feature-sidebar-extra-2

--- src/js/components/header/Header.js
+++ src/js/components/header/Header.js
@@ -1,12 +1,8 @@
 import {Component} from '../../core/Component'
 import {createHeader} from './header.template'
-import {openMenu} from './js/openMenu'
-import {mobileSearch} from './js/mobileSearch'
-import {desktopSearch} from './js/desktopSearch'
-import {showRegistryUserData} from './js/showRegistryUserData'
 
-import {info} from "./js/info";
 import {ExtraMenuItemShow} from './js/ExtraMenuItemShow'
+import {HeaderFunctions} from './js/HeaderFunctions'
 
 
 export class Header extends Component {
@@ -14,24 +10,68 @@ export class Header extends Component {
   constructor($root) {
     super($root, {
       name: 'Header',
-      listeners: ['click', 'input', 'mouseenter', 'mouseover']
+      listeners: ['click', 'input', 'mouseover']
     });
-
+    this.header = null
   }
   toHTML() {
     return createHeader()
   }
 
+  afterInitComponent() {
+    this.header = new HeaderFunctions()
+  }
+
   onClick(e) {
-    spreaderClick(e)
+
+    if(e.target.closest('article')) {
+      const element = e.target.closest('article');
+
+      switch (element.id) {
+        case 'burger-menu':
+          this.header.mobileSearchClose()
+          this.header.closeRegistryUserData()
+          this.header.infoClose()
+          this.header.menuOpen()
+          break;
+        case 'mobile-search-button-js':
+         this.header.menuClose()
+          this.header.closeRegistryUserData()
+          this.header.infoClose()
+          this.header.mobileSearchOpen()
+          break;
+        case 'desktop-search-js':
+          this.header.desktopSearchActive(element)
+          break;
+        case 'icon-info-button-js':
+          this.header.mobileSearchClose()
+          this.header.menuClose()
+          this.header.closeRegistryUserData()
+          this.header.infoOpen()
+          break;
+        case 'registry-user-mobile-button-js':
+          this.header.menuClose()
+          this.header.mobileSearchClose()
+          this.header.infoClose()
+          this.header.showRegistryUserData()
+          break;
+      }
+    }
   }
 
   onInput(e) {
-    spreaderOnInput(e)
-  }
+    if(e.target.closest('article')) {
+      const element = e.target.closest('article');
 
-  onMouseenter(e) {
-
+      switch (element.id) {
+        case 'desktop-search-js':
+          this.header.desktopSearchInputActive(e)
+          break;
+        case 'mobile-search-js':
+          this.header.mobileSearchInput(e)
+          break;
+      }
+    }
   }
 
   onMouseover(e) {
@@ -41,59 +81,7 @@ export class Header extends Component {
 
 }
 
-function spreaderOnInput(e) {
-  if(e.target.closest('article')) {
-    const element = e.target.closest('article');
 
-    switch (element.id) {
-      case 'desktop-search-js':
-        desktopSearch(e, element)
-        break;
-      case 'mobile-search-js':
-        mobileSearchInput(e, element)
-        break;
-    }
-  }
-
-
-}
-
-// переделать
-function spreaderClick(e) {
-  if(e.target.closest('article')) {
-    const element = e.target.closest('article');
-
-    switch (element.id) {
-      case 'burger-menu':
-        mobileSearch('close')
-        showRegistryUserData('close')
-        info('close')
-        openMenu()
-        break;
-      case 'mobile-search-button-js':
-        openMenu('close')
-        showRegistryUserData('close')
-        info('close')
-        mobileSearch()
-        break;
-      case 'desktop-search-js':
-        desktopSearch(e, element, 'click')
-        break;
-      case 'icon-info-button-js':
-        mobileSearch('close')
-        openMenu('close')
-        showRegistryUserData('close')
-        info()
-        break;
-      case 'registry-user-mobile-button-js':
-        openMenu('close')
-        mobileSearch('close')
-        info('close')
-        showRegistryUserData()
-        break;
-    }
-  }
-}
 
 
 
--- src/js/components/header/js/showRegistryUserData.js
+++
@@ -1,9 +0,0 @@
-export function showRegistryUserData(type = '') {
-  const elem = document.querySelector('#wrapper-user-data-view-js')
-  if(type === 'close') {
-    elem.classList.remove('wrapper-user-data-view-show')
-    return
-  }
-
-  elem.classList.toggle('wrapper-user-data-view-show')
-}
