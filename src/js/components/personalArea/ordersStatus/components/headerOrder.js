101322a3c2d58f9c8c57fbc82616f01ee67375c1
ilmirazaynetdinova <ilmirazainetdinova686@gmail.com>
2021-02-14 18:05:30 +0300

    orderPage-adaptive-1024px

--- src/js/components/personalArea/ordersStatus/components/headerOrder.js
+++ src/js/components/personalArea/ordersStatus/components/headerOrder.js
@@ -2,7 +2,6 @@ export function headerOrder() {
     return `
         <tr class="header-order">
             <td class="first-string status-1">Заказ</td>
-            <td class="first-string status-2"></td>
             <td class="first-string status-3">Количество</td>
             <td class="first-string status-4">Статус</td>
             <td class="first-string status-5">Сумма</td>
--- src/js/components/personalArea/ordersStatus/components/nameOrder.js
+++ src/js/components/personalArea/ordersStatus/components/nameOrder.js
@@ -1,29 +1,25 @@
 export function nameOrder(img,title) {
-    return `
-        <td>
-            <div class="img-order">
-                <img src="${img}" alt="">
-                <div class="title">${title}</div>
-            </div>
-        </td>
+    return `      
+        <div class="img-order">
+            <img src="${img}" alt="">
+            <div class="title">${title}</div>
+        </div>       
     `
 }
 
 export function orderName(data) {
     return `
-        <td>
-            <div class="wrapper-order-name">
-                <div class="order-name">
-                    <span class="order-title">Заказ № 12345 <span class="data">от 01.12.2020</span></span>
-                    ${characteristicOrder(data)}
-                </div>
-                <div>
-                    <a href="" class="link-see-order">
-                        Посмотреть заказ
-                    </a>
-                </div>
+        <div class="wrapper-order-name">
+            <div class="order-name">
+                <span class="order-title">Заказ № 12345 <span class="data">от 01.12.2020</span></span>
+                ${characteristicOrder(data)}
             </div>
-        </td>
+            <div>
+                <a href="" class="link-see-order">
+                    Посмотреть заказ
+                </a>
+            </div>
+        </div>     
     `
 }
 
@@ -36,5 +32,3 @@ function characteristicOrder(item) {
     return `${items.join('')}`
 }
 
-// <span class="order-title-delivery">Доставка: <span class="order-delivery">Самовывоз</span></span>
-// <span class="order-title-delivery">Получатель: <span class="order-delivery">ИП Получатель</span></span>
