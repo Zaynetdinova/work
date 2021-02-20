7b7b9ed75bb9e2345735ad7edb7ce6bd173f7411
designer <moyo.group@iMac-designer.local>
2021-02-20 13:54:38 +0300

    color-var

--- src/js/components/header/view/bottomHeader/desktopHeader.js
+++ src/js/components/header/view/bottomHeader/desktopHeader.js
@@ -3,6 +3,9 @@ import search_img from '../../../../../images/header/search.svg'
 import plus from '../../../../../images/icons/plus.svg'
 import basket from '../../../../../images/header/basket.svg'
 import like from '../../../../../images/header/like.svg'
+import women from '../../../../../images/bestsellers/1/img1.png'
+import {infoProduct} from "../../../common/infoProduct";
+import {priceContent} from "../../../common/priceContent";
 
 
 export function desktopHeader() {
@@ -47,18 +50,156 @@ export function desktopHeader() {
             </article> 
             
              ${window.userRegistry
-                ?   `<a href="/#personal-area/favorites" class="registry-user favorites">
-                                <img src=${like}>
-                      </a> `
+                ?   ` 
+                    <div class="favorite-header">
+                        <a href="/#personal-area/favorites" class="registry-user favorites">
+                            <img src=${like}>
+                        </a>
+                        <div class="favorite-dropdown">
+                            <div class="basket-dropdown-wrap">
+                                <ul>
+                                    <li>
+                                        <div class="wrapper-product">
+                                            <img class="img-product" src="${women}" alt="">
+                                            <div class="wrapper-info">
+                                                <div class="name-brand">Бренд</div>
+                                                <div class="name-product">Наименование товара</div>
+                                                ${infoProduct(infoFavorite)}
+                                                <div class="price">${priceContent('1 234,00 ₽', '-234,00 ₽', '1 234,00 ₽', true)}</div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="wrapper-product">
+                                            <img class="img-product" src="${women}" alt="">
+                                            <div class="wrapper-info">
+                                                <div class="name-brand">Бренд</div>
+                                                <div class="name-product">Наименование товара</div>
+                                                ${infoProduct(infoFavorite)}
+                                                <div class="price">${priceContent('','', '1 234,00 ₽', false)}</div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="wrapper-product">
+                                            <img class="img-product" src="${women}" alt="">
+                                            <div class="wrapper-info">
+                                                <div class="name-brand">Бренд</div>
+                                                <div class="name-product">Наименование товара</div>
+                                                ${infoProduct(infoFavorite)}
+                                                <div class="price">${priceContent('','', '1 234,00 ₽', false)}</div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="wrapper-product">
+                                            <img class="img-product" src="${women}" alt="">
+                                            <div class="wrapper-info">
+                                                <div class="name-brand">Бренд</div>
+                                                <div class="name-product">Наименование товара</div>
+                                                ${infoProduct(infoFavorite)}
+                                                <div class="price">${priceContent('','', '1 234,00 ₽', false)}</div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                                <div class="basket-dropdown-total">
+                                    <a href="/#personal-area/favorites">В ИЗБРАННОЕ</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                        `
                 : '' }   
                                 
             <div class='buy'>
                 <a href="/#personal-area/basket" class="content">
                     <img class='basket' id='basket' src=${basket} alt='basket'>
-                    <div class='basket-price' id='basket-price'>Корзина <br> пуста</div>
+                    <div class='basket-price' id='basket-price'>88 шт. /<br> 888 000 ₽</div>
                 </a>
+                <div class="basket-dropdown">
+                    <div class="basket-dropdown-wrap">
+                        <div class="basket-title">
+                             <span class="box">Корзина:</span><span class="number">88 шт. / 888 000 ₽</span>
+                        </div>
+                        <ul>
+                            <li>
+                                <div class="wrapper-product">
+                                    <img class="img-product" src="${women}" alt="">
+                                    <div class="wrapper-info">
+                                        <div class="name-brand">Бренд</div>
+                                        <div class="name-product">Наименование товара</div>
+                                        ${infoProduct(info)}
+                                        <div class="price">${priceContent('1 234,00 ₽', '-234,00 ₽', '1 234,00 ₽', true)}</div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="wrapper-product">
+                                    <img class="img-product" src="${women}" alt="">
+                                    <div class="wrapper-info">
+                                        <div class="name-brand">Бренд</div>
+                                        <div class="name-product">Наименование товара</div>
+                                        ${infoProduct(info)}
+                                        <div class="price">${priceContent('','', '1 234,00 ₽', false)}</div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="wrapper-product">
+                                    <img class="img-product" src="${women}" alt="">
+                                    <div class="wrapper-info">
+                                        <div class="name-brand">Бренд</div>
+                                        <div class="name-product">Наименование товара</div>
+                                        ${infoProduct(info)}
+                                        <div class="price">${priceContent('','', '1 234,00 ₽', false)}</div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="wrapper-product">
+                                    <img class="img-product" src="${women}" alt="">
+                                    <div class="wrapper-info">
+                                        <div class="name-brand">Бренд</div>
+                                        <div class="name-product">Наименование товара</div>
+                                        ${infoProduct(info)}
+                                        <div class="price">${priceContent('','', '1 234,00 ₽', false)}</div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="basket-dropdown-total">
+                            <a href="/#personal-area/basket">В КОРЗИНУ</a>
+                        </div>
+                    </div>
+                </div>
             </div>                              
         </div> 
      </div>
   `
 }
+
+let infoFavorite = [
+    {
+        infoType: 'ID:',
+        infoResult: '285660'
+    },
+    {
+        infoType: 'Цвет:',
+        infoResult: 'Черный'
+    },
+    {
+        infoType: 'Размеры:',
+        infoResult: '42-44, 44-46'
+    },
+]
+let info = [
+    {
+        infoType: 'ID:',
+        infoResult: '285660'
+    },
+    {
+        infoType: 'Цвет:',
+        infoResult: 'Черный'
+    },
+]
