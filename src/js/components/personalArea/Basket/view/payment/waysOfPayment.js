8114ebf429d3d373ddd8797d20cf7476ac83e240
designer <moyo.group@iMac-designer.local>
2021-03-01 16:12:24 +0300

    data-sidebar-boy

--- src/js/components/personalArea/Basket/view/payment/waysOfPayment.js
+++ src/js/components/personalArea/Basket/view/payment/waysOfPayment.js
@@ -1,4 +1,5 @@
 import cash from '../../../../../../images/icons/cash.svg'
+import {inputChoice} from "../../../../common/personalCheckedOptions";
 
 export function waysOfPayment() {
     return `
@@ -8,8 +9,43 @@ export function waysOfPayment() {
                 Способы оплаты
             </div>
             <div class="wrapper-choice-input">
-                
+                ${ChoiceInput(item)}
             </div>
         </div>
     `
 }
+
+
+function ChoiceInput(item) {
+    const items = item.map((item) => {
+        return `
+            <div class="choice">
+                <label class="radio" id="${item.id}">
+                    <input data-checkbox-input-js type="radio" name="g">
+                    <span></span>
+                    <div>${item.titleCheckbox}</div>
+                </label>
+            </div>
+        `
+    })
+    return `${items.join('')}`
+}
+
+let item = [
+    {
+        titleCheckbox: 'Оплата наличными',
+        id: 'cash-js'
+    },
+    {
+        titleCheckbox: 'Безналичная оплата',
+        id: 'nonCash-js'
+    },
+    {
+        titleCheckbox: 'Оплата картой на сайте',
+        id: 'card-js'
+    },
+    {
+        titleCheckbox: 'Оплата с депозита',
+        id: 'deposit-js'
+    },
+]
